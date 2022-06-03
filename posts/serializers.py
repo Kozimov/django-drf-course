d8a0cbfae28c7f96ea3f7292f025e2fb55b4bce5
Kozimov <kozimovziyoxiddin1415@gmail.com>
2022-06-03 15:55:24 +0500

    post drf

---
+++ posts/serializers.py
@@ -0,0 +1,10 @@
+from dataclasses import field
+from rest_framework import serializers
+from .models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = (
+            '__all__'
+        )
